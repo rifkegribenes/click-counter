a45434059d63fe6e2b702a5c938f6c665c9fffae
rifkegribenes <rifkegribenes@gmail.com>
2019-01-05 14:16:36 -0800

    counter tests passing

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,14 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 /**
  * Factory function to create a ShallowWrapper for the App component
  * @function setup
- * @param  {Object} props - Component props specific to this setup.
- * @param  {any} state - Initial state for setup.
+ * @param  {object} props - Component props specific to this setup.
+ * @param  {object} state - Initial state for setup.
  * @return {ShallowWrapper}
  */
 const setup = (props={}, state=null) => {
-	return shallow(<App {...props} />)
+	const wrapper = shallow(<App {...props} />)
+	if (state) wrapper.setState(state);
+	return wrapper;
 }
 
 /**
@@ -47,9 +49,22 @@ test('renders counter display', () => {
 });
 
 test('counter starts at 0', () => {
-
+	const wrapper = setup();
+	const initialCounterState = wrapper.state('counter');
+	expect(initialCounterState).toBe(0);
 });
 
 test('clicking button increments counter', () => {
+	const counter = 7;
+	const wrapper = setup(null, { counter });
+
+	// find button and click
+	const button = findByTestAttr(wrapper, 'increment-button');
+	button.simulate('click');
+	wrapper.update();
+
+	// find display and test value
+	const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+	expect(counterDisplay.text()).toContain(counter + 1);
 
 })
